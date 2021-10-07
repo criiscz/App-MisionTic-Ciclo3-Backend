e6a397737c83cfcd5a58b45ec503f42b2eff30dc
Cristian Sánchez <criissf15@gmail.com>
2021-10-07 12:18:50 -0500

    cambios

--- BackEnd/core/urls.py
+++ BackEnd/core/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1.0/', include('ECommerce.urls'))
+    path('api/', include('ECommerce.urls'))
 
 ]
 
