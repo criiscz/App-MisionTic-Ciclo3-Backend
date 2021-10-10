205425fe45b9d8a9ae8c3b6bc5a23c5647068824
JoelSamir <josahergo91@gmail.com>
2021-10-10 15:24:59 -0500

    Se adiciona archivo de pruebas en postman

--- BackEnd/ECommerce/views/UserView.py
+++ BackEnd/ECommerce/views/UserView.py
@@ -36,7 +36,7 @@ class UserDetailView(generics.RetrieveAPIView):
 
     def get(self, request, *args, **kwargs):
         valid_data = validate_token(request)
-        user_id = User.objects.filter(id=valid_data['user_id'], user_type='Seller').first()
+        user_id = User.objects.filter(account_id=valid_data['user_id'], user_type='Seller').first()
         if user_id:
             queryset = User.objects.filter(id=kwargs['id_search'])
             return Response(UserSerializer(queryset).data, status=200)
